59059cb6d2967e842b44e5492502e75ee5b8d707
pjwriteserrors <philipp.j6y@gmail.com>
2025-06-18 12:18:06 +0200

    clean main

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,6 +7,8 @@ public class Main {
         //! Tests, nicht löschen, lieber auskommentieren wenn das stört
         //! Jo, wenn die Liste größer ist auch schön beim print das i < auf einen höeren int stellen.
         //? was wenn nicht
+
+        // make defaults
         ExtendedLinkedList list = new ExtendedLinkedList();
         ExtendedLinkedList list2 = new ExtendedLinkedList();
 
@@ -18,18 +20,23 @@ public class Main {
         list2.addLast(5);
         list2.addLast(6);
 
+        //* AddI test
         list.addI(1000, 1);
-        System.out.println("addI an Stelle 1: \n");
+        System.out.println("\u001B[32m" + "addI an Stelle 1:" + "\u001B[0m");
         for (int i = 0; i < 4; i++) {
             System.out.println(list.get(i));
         }
+        System.out.println("\n");
+
+        //* RemoveI test
         list.removeI(2);
 
         list.combineLists(list2);
-        System.out.println("removeI an Stelle 2 und combineLists \n");
+        System.out.println("\u001B[32m"+ "removeI an Stelle 2 und combineLists"+ "\u001B[0m");
         for (int i = 0; i < 6; i++) {
             System.out.println(list.get(i));
         }
+        System.out.println("\n");
 
 
         ExtendedLinkedList list3 = new ExtendedLinkedList();
@@ -42,11 +49,11 @@ public class Main {
 
         list3.removeDuplicates();
 
-        System.out.println("Remove duplicates \n");
+        System.out.println("\u001B[32m"+"Remove duplicates:"+ "\u001B[0m");
         for (int i = 0; i < 4; i++) {
             System.out.println(list3.get(i));
         }
-
+        System.out.println("\n");
 
     }
 }
